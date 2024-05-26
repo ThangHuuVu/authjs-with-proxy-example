00b35a5334d6d884a88270b4843e292d7a3112c5
Thang Vu <hi@thvu.dev>
2024-05-26 22:22:56 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,10 @@ export default function Index() {
           </CustomLink>
           ) is deployed on Vercel.
         </p>
+        <p className="text-gray-500 dark:text-gray-400">
+          After signin in, check the cookies in the DevTool -&gt; Application
+          tab to see the session cookie being applied to this domain.
+        </p>
         <Link
           className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
           href="/authjs"
