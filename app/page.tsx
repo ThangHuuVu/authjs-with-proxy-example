6a7dd3ed821fb96d091b833dcd0a9f978c97f7ae
Thang Vu <hi@thvu.dev>
2024-05-26 21:16:39 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Index() {
           </p>
           <Link
             className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-            href="/authjs/signin"
+            href="/authjs"
           >
             <LogIn className="mr-2 h-5 w-5" />
             Sign In with Auth.js
